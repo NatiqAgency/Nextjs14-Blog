05ac402108b28caf4e40c1206dd2c2669e551e83
Benjamin BENOIT <shankiflang@gmail.com>
2023-11-10 19:35:38 +0100

    Update

--- src/app/api/auth/[…nextauth]/route.ts
+++ src/app/api/auth/[…nextauth]/route.ts
@@ -1,5 +1,20 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import prisma from '@/lib/prisma';
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        })
+    ]
+};
+
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
