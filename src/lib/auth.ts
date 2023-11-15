4759137bb123544168c6e90c635dd4d83ce0ef22
Benjamin BENOIT <shankiflang@gmail.com>
2023-11-15 14:22:56 +0100

    Update prisma adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github';
 import prisma from './prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 
 export const {
     handlers: { GET, POST },
